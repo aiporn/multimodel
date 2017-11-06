383202b19a573c3f6d13048e9d920cc79c84d185
AI Porn <openpornai@gmail.com>
2017-11-05 19:16:40 -0800

    deal with empty hotspots

--- multimodel/data.py
+++ multimodel/data.py
@@ -192,7 +192,7 @@ class DataDir:
           timestamp and returns a view count.
         """
         metadata = self._id_to_meta[video_id]
-        if 'hotspots' not in metadata:
+        if 'hotspots' not in metadata or len(metadata['hotspots']) < 2:
             return None
         times = [i * metadata['duration'] / (len(metadata['hotspots'])-1)
                  for i, _ in enumerate(metadata['hotspots'])]
