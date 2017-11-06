attempt to fix various numerical issues
Got NaNs during training. Now I've made the following changes:

 * Remove outliers from hotspot data
 * Don't divide by hotspot magnitudes
 * Use cross-entropy for like loss
 * Clip views before taking log

multimodel/hotspots.py
@@ -40,7 +40,7 @@ class HotspotPredictor:
         return self._predictions
 
     def loss(self, actual_intensities,
-             rescale_fn=lambda x: tf.clip_by_value(tf.log(x), 0, np.inf)):
+             rescale_fn=lambda x: tf.log(tf.clip_by_value(x, 1, np.inf))):
         """
         Compute the prediction loss.
 
@@ -50,12 +50,11 @@ class HotspotPredictor:
             neural-friendly range.
 
         Returns:
-          A 0-D Tensor representing the mean cosine distance between the
-            actual and target histograms.
+          A 0-D Tensor representing the distance between the actual and target
+            histograms.
         """
         actual = _subtract_mean(rescale_fn(actual_intensities))
-        prods = _normalize_mag(self.predictions) * _normalize_mag(actual)
-        return 1 + tf.negative(tf.reduce_mean(tf.reduce_sum(prods, axis=1)))
+        return tf.reduce_mean(tf.square(self.predictions - actual))
 
 def _subtract_mean(vector_batch):
     """
@@ -63,9 +62,3 @@ def _subtract_mean(vector_batch):
     """
     means = tf.expand_dims(tf.reduce_mean(vector_batch, axis=1), axis=1)
     return vector_batch - means
-
-def _normalize_mag(vector_batch):
-    """
-    For each vector in a batch, divide by the norm.
-    """
-    return vector_batch / tf.norm(vector_batch, axis=1, keep_dims=True)

multimodel/popularity.py
@@ -10,7 +10,7 @@ import numpy as np
 import tensorflow as tf
 
 # Bring the view loss down to a reasonable magnitude.
-_VIEW_LOSS_SCALE = 1 / (math.log(1e5) ** 2)
+_VIEW_LOSS_SCALE = 1 / (math.log(1e4) ** 2)
 
 class PopularityPredictor:
     """
@@ -38,7 +38,7 @@ class PopularityPredictor:
         return self._views
 
     def loss(self, actual_like_frac, actual_views,
-             rescale_fn=lambda x: tf.clip_by_value(tf.log(x), 0, np.inf)):
+             rescale_fn=lambda x: tf.log(tf.clip_by_value(x, 1, np.inf))):
         """
         Compute the prediction loss.
 
@@ -51,6 +51,8 @@ class PopularityPredictor:
         Returns:
           A 0-D Tensor representing the mean prediction loss.
         """
-        like_loss = tf.reduce_mean(tf.square(actual_like_frac - self.like_frac))
+        like_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=actual_like_frac,
+                                                            logits=self._like_frac)
+        like_loss = tf.reduce_mean(like_loss)
         view_loss = tf.reduce_mean(tf.square(rescale_fn(actual_views) - self.views))
         return like_loss + _VIEW_LOSS_SCALE * view_loss